7485b1facef6969d6c5ef28ece941da43826913d
Merrouche <theo.merrouche@etu.uca.fr>
2025-10-06 16:51:52 +0200

    ajout de la bar de recherche

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -8,17 +8,32 @@ import { useState } from "react"
 function Home(){
     const [searchQuery, setSearchQuery] = useState ("");{/* set pour changer la valeur et searchQuery qui est la valeur elle meme,
                                                              permet de notifier la vue */}
+    const [filteredMachines, setFilteredMachines] = useState([]);{/* pour le futur filtrage */}
+        
     const machines =[
         {id: 1, title: "deadlift", image : "/images/sdt.jpg", alt : "soulever de terre"},
         {id: 2, title: "benchpress", image : "/images/developpecouche.jpeg", alt : " développé couché"},
         {id: 3, title: "squat", image : "/images/squat.jpg", alt : "Squat"},
         {id: 4, title: "incline bench" , image : "/images/developperincline.png", alt : "développé incliné"},
     ]
-
     const handleSearch = (e) => {
-        alert(searchQuery) 
+        e.preventDefault() // empeche le rechargement de la page
+        if (searchQuery.trim() === "") {
+            // si la barre de recherche est vide, on affiche tout
+            setFilteredMachines(machines);
+        }
+        setFilteredMachines(
+            machines.filter(machine =>
+                machine.title.toLowerCase().startsWith(searchQuery.trim().toLocaleLowerCase())
+            )
+        );
         setSearchQuery("") // met ce qui est a l'intérieur a "" pour faire un clear meme en backend
     }
+
+ 
+    const machinesToDisplay = filteredMachines.length > 0 
+        ? filteredMachines
+        : machines;
     return( 
     <div className="Home">
         <form onSubmit={handleSearch} className="search-form">
@@ -31,7 +46,7 @@ function Home(){
             <button type="submit" className="submit-btn">🔍</button>
         </form>
         <div className="machine-list">
-            {machines.map(machine =>(  // .map parcours toute la liste machines et vas donner l'objet machine qui est égale a machineCard ...
+            {machinesToDisplay.map(machine =>(  // .map parcours toute la liste machines et vas donner l'objet machine qui est égale a machineCard ...
                 <MachineCard machine={machine} key={machine.id}/>
             ))}
         </div>
